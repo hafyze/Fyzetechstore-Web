b4b44f8771d6a0c25bb804386a8ed9fb57190d0c
hafyze <84822836+hafyze@users.noreply.github.com>
2022-07-10 23:46:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//TOPNAV icon
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+function myFunction() {
+var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+     } else {
+     x.className = "topnav";
+     }
+}
+
+
+myFunction = document.getElementById("footer")
+{
+    
+}
